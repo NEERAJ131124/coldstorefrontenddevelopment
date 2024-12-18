1ce468744d96914b01242a587471e22390f238c0
Neeraj <neeraj@antheminfotech.in>
2024-12-18 17:19:57 +0530

    test1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { ToastContainer } from 'react-toastify';
 import Routers from './Routes';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// testing
+
+
 function App() {
   const queryClient = new QueryClient()
   return (
